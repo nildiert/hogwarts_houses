2a6194b85c9a047452dbf2011e905367b5a7603a
nildiert <niljordan23@gmail.com>
2020-10-21 12:36:48 -0500

    [FEATURE] Added teacher Service

---
+++ src/app/models/teacher.ts
@@ -0,0 +1,22 @@
+export interface Teacher {
+    name: string;
+    species?: string;
+    gender?: string;
+    house?: string;
+    dateOfBirth?: string;
+    yearOfBirth?: number;
+    ancestry?: string;
+    eyeColour?: string;
+    hairColour?: string;
+    wand?: {
+        wood?: string;
+        core?: string;
+        length?: string;
+    };
+    patronus: string,
+    hogwartsStudent?: false,
+    hogwartsStaff?: true,
+    actor?: string,
+    alive?: true,
+    image: "http://hp-api.herokuapp.com/images/mcgonagall.jpg"
+}
--- src/app/services/teachers.service.ts
+++ src/app/services/teachers.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Teacher } from "../models/teacher";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeachersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTeachers() {
+    return this.http.get<Teacher[]>('http://hp-api.herokuapp.com/api/characters/staff');
+  }
 }
