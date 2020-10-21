235a21a98441b19156e9bf1f41d14eb0eba74c23
nildiert <niljordan23@gmail.com>
2020-10-20 19:01:17 -0500

    [FEATURE] Added characters, students and teachers components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { CharactersComponent } from './characters/characters.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: AppComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'teachers', component: TeachersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { StudentsComponent } from './students/students.component';
+import { TeachersComponent } from './teachers/teachers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    StudentsComponent,
+    TeachersComponent
   ],
   imports: [
     BrowserModule,
